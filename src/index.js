b7673cd74664468d54cf083952852923fc6d4068
Kian Davoudi-Rad <kiandr@KIAN.hitronhub.home>
2018-10-28 11:14:29 -0700

    adding the new component and event listener

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './bootstrap.min.css';
 import App from './App';
+import Radware from './Radware';
+import './bootstrap.min.css';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<Radware />, document.getElementById('root'));
 
+function showAlert(onSuccess, onFail) {
+    // Get the current 'global' time from an API using Promise
+    return new Promise((resolve, reject) => {setTimeout(()=> {resolve('show');}, 100);
+    })
+  }
+  showAlert()
+    .then(currentTime => showAlert())
+    .then(currentTime => {
+        if(currentTime ==='show')
+      console.log('The current time is: ' + currentTime + new Date());
+      var t = new Radware(currentTime);
+
+      
+      return true;
+    })
+    .catch(err => console.log('There was an error:' + err))
+    
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
